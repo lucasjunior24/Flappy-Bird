63abed6c519f52e80bb843add51ee4e6b84d1f09
lucasjunior24 <53240060+lucasjunior24@users.noreply.github.com>
2020-06-09 14:37:22 -0500

    ajustando o codigo para rodar dentro do tkinter

--- flappy.py
+++ flappy.py
@@ -1,26 +1,23 @@
-import pygame
-import random
-from pygame.locals import *
-
 # Importar Bibliotecas
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
 import DataBaser
+import tkinter as tk
+import os
+import random
+import pygame
+
 
 # Criar janela
 jan = Tk()
 jan.title("DP Systems - Acess Panel")
-jan.geometry("410x500")
+jan.geometry("427x582")
 jan.configure(background="white")
 jan.resizable(width=False, height=False)
 jan.attributes("-alpha", 1.9)
 
 # Widgets
-
-
-def close():
-    jan.destroy()
 
 
 RightFrame = Frame(jan, width=410, height=500,
@@ -52,11 +49,13 @@ def Login():
     """, (User, Pass))
     print("Selecionou")
     VerifyLogin = DataBaser.cursor.fetchone()
-    try:
-        if(User in VerifyLogin and Pass in VerifyLogin):
-            messagebox.showinfo(title="Login Info",
-                                message="Acesso Confirmado, Bem vindo!", command=close())
-    except:
+
+    if(User in VerifyLogin and Pass in VerifyLogin):
+        messagebox.showinfo(
+            title="Login Info", message="Acesso Confirmado, Bem vindo!")
+        close()
+        open()
+    else:
         messagebox.showinfo(title="Login Info", message="Acesso Negado!")
 
 
@@ -125,175 +124,205 @@ RegisterButton = ttk.Button(
     RightFrame, text="Register", width=20, command=Register)
 RegisterButton.place(x=120, y=320)
 
+# variaveis globais
+
+baseX = 0
+passaroX = 0
+passaroY = 0
+passaroVel = 0
+cano = []
+pontos = 0
+contarPontos = False
+
+pygame.font.init()
+
+
+def close():
+    jan.destroy()
+
+
+# def jogar():
+#     tela2 = Tk()
+#     tela2.title("DP Systems - Acess Panel")
+#     tela2.geometry("300x300")
+
+
+def open():
+
+    tela = tk.Tk()
+    # creates embed frame for pygame window
+    moudura = tk.Frame(tela, width=427, height=582)
+
+    # moudura.grid(columnspan=(600), rowspan=500)  # Adds grid
+    moudura.pack(side=LEFT)  # packs window to the left
+
+    os.environ['SDL_WINDOWID'] = str(moudura.winfo_id())
+    os.environ['SDL_VIDEODRIVER'] = 'windib'
+
+    # iniciação
+    pygame.display.init()
+
+    #
+
+    FPS = 60  # FHASHS POR SEGUNDO
+    JANELA = pygame.display.set_mode((427, 582))
+
+    VEL_AVANCO = 4
+    FONTE = pygame.font.SysFont("Comic Sans MS", 50, True)
+    YELLOW = (255, 255, 0)
+    # desenha
+
+    global baseX, passaroX, passaroY, passaroVel, cano, pontos
+    baseX = 0
+    passaroX = 0
+    passaroY = 0
+    passaroVel = 0
+    pontos = 0
+    cano = []
+    contarPontos = False
+
+    # carregando imagens
+    base = pygame.image.load("img/base2.png")
+    gameOver = pygame.image.load("img/gameOver.png")
+    bird = pygame.image.load("img/bird.png")
+    fundo = pygame.image.load("img/fundoQuadrado.gif")
+    pipe = pygame.image.load("img/pipe.png")
+    pipeCima = pygame.transform.flip(pipe, False, True)  # gira o cano
+    # DEFINIR CLASSE CANO
+
+    class Canos:
+        def __init__(self):
+            super().__init__()
+            self.x = 320
+            self.y = random.randint(-75, 150)
+
+        def desenhar(self):
+
+            self.x -= VEL_AVANCO
+            JANELA.blit(pipeCima, (self.x + 70, self.y - 190))
+            JANELA.blit(pipe, (self.x + 70, self.y + 290))
+
+        def colidir(self, bird, passaroX, passaroY, pipe):
+            tolerancia = 5
+            passaroLarX = passaroX + bird.get_width()  # largura
+            passaroLX = passaroX
+            passaroAY = passaroY
+            passaroAltY = passaroY + bird.get_height()  # altura
+            pipeLarX = self.x + pipe.get_width() + 70
+            pipeLX = self.x + 70
+            pipeAY = self.y + 128
+            pipeAltY = self.y + 300
+            if passaroLX < pipeLarX and passaroLarX > pipeLX:
+                if passaroAY < pipeAY or passaroAltY > pipeAltY:
+                    vocePerdeu()
+
+        def contarPontos(self, passaroX, pipe):
+            passaroLarX = passaroX + bird.get_width()  # largura
+            passaroLX = passaroX
+            pipeLarX = self.x + pipe.get_width()
+            pipeLX = self.x
+            if passaroLarX < pipeLarX and passaroLX > pipeLX:
+                return True
+
+    def desenhar():
+
+        # jANELA = tk.Frame(tela)
+        JANELA.blit(fundo, (0, 0))  # colocando o fundo
+        for canos in cano:
+            canos.desenhar()
+        # baseX = 0
+        # passaroX = 80
+        # passaroY = 150
+        JANELA.blit(base, (0, 480))
+        JANELA.blit(bird, (passaroX, passaroY))
+        pontuacao = FONTE.render(str(pontos), True, YELLOW)
+        JANELA.blit(pontuacao, (214, 70))
+
+    def atualizarTela():
+        pygame.display.update()
+        pygame.time.Clock().tick(FPS)
+
+    def inicializarVariaveis():
+        global baseX, passaroX, passaroY, passaroVel, cano, pontos
+        baseX = 0
+        passaroX = 80
+        passaroY = 150
+        passaroVel = 0
+        pontos = 0
+        cano = []
+        cano.append(Canos())
+        # contarPontos = False
+
+    def vocePerdeu():
+        JANELA.blit(gameOver, (0, 60))
+        atualizarTela()
+        verificar = True
+        while verificar:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        inicializarVariaveis()
+                        # baseX = 0
+                        # passaroX = 80
+                        # passaroY = 150
+                        # # passaroVel = 0
+                        # pontos = 0
+                        # cano = []
+                        # cano.append(Canos())
+                        global passaroVel
+                        passaroVel = 10
+                        verificar = False  # passando o verifica pra false, o jogador podew jogar novamente
+
+    inicializarVariaveis()
+
+    while True:
+        tela.update()
+        desenhar()
+        atualizarTela()
+        passaroVel -= 1
+        passaroY -= passaroVel
+        baseX -= VEL_AVANCO
+        if baseX <= -5:
+            baseX = 0
+        if passaroY >= 480:
+            vocePerdeu()
+
+        # aumenta a quantidade de canos
+        if cano[-1].x <= 140:
+            cano.append(Canos())
+
+        if len(cano) >= 4:
+            del cano[0]
+
+        if not contarPontos:
+            for canos in cano:
+                if canos.contarPontos(passaroX, pipe):
+                    contarPontos = True
+                    break
+
+        if contarPontos:
+            contarPontos = False
+            for canos in cano:
+                if canos.contarPontos(passaroX, pipe):
+                    contarPontos = True
+                    break
+            if not contarPontos:
+                pontos += 1
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    passaroVel = 10
+
+        for canos in cano:
+            canos.colidir(bird, passaroX, passaroY, pipe)
+
+    tela.mainloop()
+
+
 jan.mainloop()
-
-
-SCREEN_WIDTH = 350
-SCREEN_HEIGHT = 700
-SPEED = 10
-GRAVITY = 1
-GAME_SPEED = 10
-
-GROUND_WIDTH = 2 * SCREEN_WIDTH
-GROUND_HEIGHT = 100
-
-PIPE_WIDTH = 80
-PIPE_HEIGHT = 500
-
-PIPE_GAP = 200
-
-
-class Bird(pygame.sprite.Sprite):
-
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-
-        self.images = [pygame.image.load('bluebird-upflap.png').convert_alpha(),
-                       pygame.image.load(
-                           'bluebird-midflap.png').convert_alpha(),
-                       pygame.image.load('bluebird-downflap.png').convert_alpha()]
-
-        self.speed = SPEED
-
-        self.current_image = 0
-
-        self.image = pygame.image.load('bluebird-upflap.png').convert_alpha()
-        self.mask = pygame.mask.from_surface(self.image)
-
-        self.rect = self.image.get_rect()
-        self.rect[0] = SCREEN_WIDTH / 2
-        self.rect[1] = SCREEN_HEIGHT / 2
-
-    def update(self):
-        self.current_image = (self.current_image + 1) % 3
-        self.image = self.images[self.current_image]
-
-        self.speed += GRAVITY
-
-        # Update height
-        self.rect[1] += self.speed
-
-    def bump(self):
-        self.speed = -SPEED
-
-
-class Pipe(pygame.sprite.Sprite):
-
-    def __init__(self, inverted, xpos, ysize):
-        pygame.sprite.Sprite.__init__(self)
-
-        self.image = pygame.image.load('pipe-red.png').convert_alpha()
-        self.image = pygame.transform.scale(
-            self.image, (PIPE_WIDTH, PIPE_HEIGHT))
-
-        self.rect = self.image.get_rect()
-        self.rect[0] = xpos
-
-        if inverted:
-            self.image = pygame.transform.flip(self.image, False, True)
-            self.rect[1] = - (self.rect[3] - ysize)
-        else:
-            self.rect[1] = SCREEN_HEIGHT - ysize
-
-        self.mask = pygame.mask.from_surface(self.image)
-
-    def update(self):
-        self.rect[0] -= GAME_SPEED
-
-
-class Ground(pygame.sprite.Sprite):
-
-    def __init__(self, xpos):
-        pygame.sprite.Sprite.__init__(self)
-
-        self.image = pygame.image.load('base.png').convert_alpha()
-        self.image = pygame.transform.scale(
-            self.image, (GROUND_WIDTH, GROUND_HEIGHT))
-
-        self.mask = pygame.mask.from_surface(self.image)
-
-        self.rect = self.image.get_rect()
-        self.rect[0] = xpos
-        self.rect[1] = SCREEN_HEIGHT - GROUND_HEIGHT
-
-    def update(self):
-        self.rect[0] -= GAME_SPEED
-
-
-def is_off_screen(sprite):
-    return sprite.rect[0] < -(sprite.rect[2])
-
-
-def get_random_pipes(xpos):
-    size = random.randint(100, 300)
-    pipe = Pipe(False, xpos, size)
-    pipe_inverted = Pipe(True, xpos, SCREEN_HEIGHT - size - PIPE_GAP)
-    return (pipe, pipe_inverted)
-
-
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-BACKGROUND = pygame.image.load('background-day.png')
-BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDTH, SCREEN_HEIGHT))
-
-bird_group = pygame.sprite.Group()
-bird = Bird()
-bird_group.add(bird)
-
-ground_group = pygame.sprite.Group()
-for i in range(2):
-    ground = Ground(GROUND_WIDTH * i)
-    ground_group.add(ground)
-
-pipe_group = pygame.sprite.Group()
-for i in range(2):
-    pipes = get_random_pipes(SCREEN_WIDTH * i + 800)
-    pipe_group.add(pipes[0])
-    pipe_group.add(pipes[1])
-
-
-clock = pygame.time.Clock()
-
-while True:
-    clock.tick(30)
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-
-        if event.type == KEYDOWN:
-            if event.key == K_SPACE:
-                bird.bump()
-
-    screen.blit(BACKGROUND, (0, 0))
-
-    if is_off_screen(ground_group.sprites()[0]):
-        ground_group.remove(ground_group.sprites()[0])
-
-        new_ground = Ground(GROUND_WIDTH - 20)
-        ground_group.add(new_ground)
-
-    if is_off_screen(pipe_group.sprites()[0]):
-        pipe_group.remove(pipe_group.sprites()[0])
-        pipe_group.remove(pipe_group.sprites()[0])
-
-        pipes = get_random_pipes(SCREEN_WIDTH * 2)
-
-        pipe_group.add(pipes[0])
-        pipe_group.add(pipes[1])
-
-    bird_group.update()
-    ground_group.update()
-    pipe_group.update()
-
-    bird_group.draw(screen)
-    pipe_group.draw(screen)
-    ground_group.draw(screen)
-
-    pygame.display.update()
-
-    if (pygame.sprite.groupcollide(bird_group, ground_group, False, False, pygame.sprite.collide_mask) or
-            pygame.sprite.groupcollide(bird_group, pipe_group, False, False, pygame.sprite.collide_mask)):
-        # Game over
-        input()
-        break
